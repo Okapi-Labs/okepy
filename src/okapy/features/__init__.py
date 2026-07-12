9e5607cc81c68b9cc439b43a1f7aedb9af642397
jayo14 <ayobamidayo2007@gmail.com>
2026-07-12 23:01:29 +0100

    register s3 and cloudinary features

--- src/okapy/features/__init__.py
+++ src/okapy/features/__init__.py
@@ -8,24 +8,30 @@ from okapy.features.jwt import JWTFeature
 from okapy.features.postgres import PostgresFeature
 from okapy.features.redis import RedisFeature
 from okapy.features.refresh import RefreshTokenFeature
+from okapy.features.cloudinary import CloudinaryFeature
+from okapy.features.s3 import S3Feature
 from okapy.features.social import SocialFeature
 
 register_feature(AuthFeature())
 register_feature(CeleryFeature())
+register_feature(CloudinaryFeature())
 register_feature(DockerFeature())
 register_feature(JWTFeature())
 register_feature(PostgresFeature())
 register_feature(RedisFeature())
 register_feature(RefreshTokenFeature())
+register_feature(S3Feature())
 register_feature(SocialFeature())
 
 __all__ = [
     "AuthFeature",
     "CeleryFeature",
+    "CloudinaryFeature",
     "DockerFeature",
     "JWTFeature",
     "PostgresFeature",
     "RedisFeature",
     "RefreshTokenFeature",
+    "S3Feature",
     "SocialFeature",
 ]
