a87171c121a4f0dd8dac2d0516f003690891d04d
jayo14 <ayobamidayo2007@gmail.com>
2026-07-12 23:03:26 +0100

    fix import order

--- src/okapy/features/__init__.py
+++ src/okapy/features/__init__.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from okapy.core.registry import register_feature
 from okapy.features.auth import AuthFeature
 from okapy.features.celery import CeleryFeature
+from okapy.features.cloudinary import CloudinaryFeature
 from okapy.features.docker import DockerFeature
 from okapy.features.jwt import JWTFeature
 from okapy.features.postgres import PostgresFeature
 from okapy.features.redis import RedisFeature
 from okapy.features.refresh import RefreshTokenFeature
-from okapy.features.cloudinary import CloudinaryFeature
 from okapy.features.s3 import S3Feature
 from okapy.features.social import SocialFeature
 
